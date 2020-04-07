98944c762480a16cc71dcccafdff1e0135d5a3b9
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-04-07 13:19:29 +0300

    Also done the Highest and lowest task in C#.

---
+++ Highest and Lowest/Solution  in C#/HighestAndLowest/HighestAndLowest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        string[] numberArray = numbers.Split(' ');
+        List<int> intArray = new List<int>();
+        foreach (var num in numberArray)
+        {
+            int newNum = int.Parse(num);
+            intArray.Add(newNum);
+        }
+        int max = intArray.Max();
+        int min = intArray.Min();
+
+        return max + " " + min;
+    }
+
+}
