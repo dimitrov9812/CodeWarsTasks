3d39c3ed97e3980eca2aa0c0d5f01238e08a0c09
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-04-07 13:24:17 +0300

    Added comments to both my solutions for Highest and Lowest task

--- Highest and Lowest/Solution  in C#/HighestAndLowest/HighestAndLowest/Program.cs
+++ Highest and Lowest/Solution  in C#/HighestAndLowest/HighestAndLowest/Program.cs
@@ -6,16 +6,23 @@ public static class Kata
 {
     public static string HighAndLow(string numbers)
     {
+        // Split the user input by space and save it in a new string array
         string[] numberArray = numbers.Split(' ');
+        // Create a list for later to store our parsed to integer elements from numberArray
         List<int> intArray = new List<int>();
+        // Start a foreach loop 
         foreach (var num in numberArray)
         {
+            // Parsing every element to int
             int newNum = int.Parse(num);
+            // Add it to the list
             intArray.Add(newNum);
         }
+        // Using the Linq functions to easily find the maximum and minimum value of the list
         int max = intArray.Max();
         int min = intArray.Min();
 
+        // Return the Max and Min value which we store in max and min variable
         return max + " " + min;
     }
 
--- Highest and Lowest/Solution in JS/script.js
+++ Highest and Lowest/Solution in JS/script.js
@@ -1,7 +1,10 @@
 function highAndLow(numbers){
+	//declare array where we split the given input by space and each element is being parsed into an integer
     let numbersArray = numbers.split(" ").map(function(i){
         return parseInt(i, 10);
     });
+	// Use Math.max and Math.min function to find the max and min value of element in the numbersArray
     return Math.max(...numbersArray)+" "+Math.min(...numbersArray);
   }
+//Print the output
   console.log(highAndLow("4 4 4 6"));
