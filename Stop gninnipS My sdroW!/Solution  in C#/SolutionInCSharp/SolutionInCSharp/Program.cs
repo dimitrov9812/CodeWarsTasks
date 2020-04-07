fed11a0c6543cd4e19aaf1817bbd7fb1f7aea9fa
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-04-07 11:28:47 +0300

    Added the solution for the task in C#.

---
+++ Stop gninnipS My sdroW!/Solution  in C#/SolutionInCSharp/SolutionInCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static string SpinWords(string sentence)
+    {
+        string[] splittedSentence = sentence.Split(" ");
+        string builder = "";
+
+        for (int i = 0; i < splittedSentence.Length; i++)
+        {
+            string currentWord = splittedSentence[i];
+            if (currentWord.Length >= 5)
+            {
+                char[] chars = currentWord.ToCharArray();
+                Array.Reverse(chars);
+                string newWord = string.Join("", chars);
+                if (i == splittedSentence.Length - 1)
+                {
+                    builder += newWord;
+                }
+                else
+                {
+                    builder += newWord + " ";
+                }
+
+            }
+            else
+            {
+                if (i == splittedSentence.Length - 1)
+                {
+                    builder += currentWord;
+                }
+                else
+                {
+                    builder += currentWord + " ";
+                }
+            }
+        }
+        return builder;
+    }
+}
